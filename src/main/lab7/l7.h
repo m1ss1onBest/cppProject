f48d7ec17673049a50279a3dfe87ab7d5bd64482
m1ss1onBest <138273721+m1ss1onBest@users.noreply.github.com>
2023-11-07 16:55:19 +0200

    add l1q1

--- src/main/lab7/l7.h
+++ src/main/lab7/l7.h
@@ -1,6 +1,8 @@
 ﻿//l7.h
 #pragma once
 
+
+
 void L7Q1();
 void L7Q2();
 void L7Q3();
--- src/main/lab7/l7q1.cpp
+++ src/main/lab7/l7q1.cpp
@@ -1,1 +1,52 @@
-﻿
+﻿#include <iostream>
+#include "l7.h"
+
+constexpr double calcError = 0.01;
+constexpr double pi = 3.14;
+
+bool divisionBy0 = false;
+double calcX(const double z)
+{
+    //[0.99; 1.01]
+    if(z <= 1 + calcError && z >= 1 - calcError)
+    {
+        std::cout << "division by 0 ";
+        divisionBy0 = true;
+        return 1;
+    }
+    else
+    {
+        divisionBy0 = false;
+    }
+    
+    if(z <= 0)
+    {
+        return pow(z, 2) + 5;
+    }
+    return 1 / sqrt(z - 1);
+}
+
+double calcY(const double x)
+{
+    return pow(sin(pow(x, 2) - 1), 3) + log(fabs(x) + exp(x));
+}
+
+void L7Q1()
+{
+    while (true)
+    {
+        double z;
+        std::cout << "enter> ";
+        std::cin >> z;
+
+        const double x = calcX(z);
+        if(divisionBy0)
+        {
+            std::cout << "division by 0" << std::endl;
+            continue;
+        }
+
+        const double y = calcY(x);
+        std::cout << 'z' << z << "\tx" << x << "\ty" << y << std::endl;
+    }
+}
